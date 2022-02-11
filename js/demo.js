86c94d9d6d0c31b612192ce2e80220a0bcb93893
only_狗帅 <778308168@qq.com>
2022-02-11 11:27:28 +0800

    仿星巴克

---
+++ js/demo.js
@@ -0,0 +1,25 @@
+$('#showBtn').on('click', function () {
+    $('.leftCon .menu').removeClass('hide').addClass('show');
+    shadowMenu();
+})
+
+$('#closeBtn').on('click', function () {
+    $('.leftCon .menu').removeClass('show').addClass('hide');
+    shadowMenu();
+})
+
+$(window).resize(shadowMenu);
+
+
+// 当菜单弹出的时候，不要让body出现滚动条，当菜单消失的时候再让body出现滚动条
+function shadowMenu () {
+    // 只有当屏幕尺寸小于992，以及menu菜单是显示的状态，这时才要干掉body的滚动条
+    if ($(window).innerWidth() <= 992 && $('.menu').hasClass('show')) {
+        // console.log(1);
+        $('body').css('overflow', 'hidden');
+        $('#rightContent').removeClass('rightContentShow').addClass('rightContentHide');
+    } else {
+        $('body').css('overflow', 'auto');
+        $('#rightContent').removeClass('rightContentHide').addClass('rightContentShow');
+    }
+}
